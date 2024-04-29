5122dd38be05f11006d6d034ae57d9607a95fb7f
Jared Davidson (Archetapp) <archetapp@gmail.com>
2024-04-29 14:52:00 -0600

    Initial commit

---
+++ VerifiedBadge/VerifiedBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerifiedBadgeApp.swift
+//  VerifiedBadge
+//
+//  Created by Jared Davidson on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct VerifiedBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
